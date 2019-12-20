b2a286eb99d088ad1fe280fc872dc1d4d977ba64
Christopher Lamm <chris@theantichris.com>
2019-12-20 08:27:50 -0500

    more clean up

--- maze.py
+++ maze.py
@@ -3,6 +3,8 @@ from room import Room
 class Maze:
     def __init__(self):
         self.rooms = {}
+        self.path_total = 0
+        self.current_room_name = 'entrance'
 
     def add_room(self, room):
         self.rooms[room.name] = room
@@ -19,38 +21,30 @@ class Maze:
             print("enter {} for {}: {} cost".format(key, connected_room, weight))
         return valid_choices
 
-    def move_to_room(self, current_room, choice, path_total):
+    def move_to_room(self, current_room, choice):
         for connected_room in current_room.paths.keys():
             if connected_room.startswith(choice):
-                current_room_name = self.rooms[connected_room].name
-                path_total += current_room.paths[connected_room]
+                self.current_room_name = self.rooms[connected_room].name
+                self.path_total += current_room.paths[connected_room]
 
-        print("\n***You are now in the {} ***\n".format(current_room_name))
-        return current_room_name, path_total
+        print("\n***You are now in the {} ***\n".format(self.current_room_name))
     
     def explore_maze(self):
         print("Exploring the maze...\n")
+        print("You are in the {}\n".format(self.current_room_name))
 
-        path_total = 0
-        current_room_name = "entrance"
-
-        print("You are in the {}\n".format(current_room_name))
-
-        while current_room_name != "treasure room":
-            current_room = self.rooms[current_room_name]
+        while self.current_room_name != "treasure room":
+            current_room = self.rooms[self.current_room_name]
             valid_choices = self.build_choices(current_room)
-
-            print("\nYou have accumulated: {} cost".format(path_total))
-
+            print("\nYou have accumulated: {} cost".format(self.path_total))
             choice = input("\nWhich room do you move to? ")
 
             if choice not in valid_choices:
                 print("Please select from these letters: {}".format(valid_choices))
             else:
-                current_room_name, path_total = self.move_to_room(current_room, choice, path_total)
+                self.move_to_room(current_room, choice)
 
-        print("You made it to the treasure room with {} cost".format(path_total))
-
+        print("You made it to the treasure room with {} cost".format(self.path_total))
 
     def print_map(self):
         print("\nMAZE LAYOUT\n")
